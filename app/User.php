2b600e68ce90d522e60b2cc7c37cc4f162d5d1bd
Jonlle <jonlle19@gmail.com>
2020-03-08 00:07:48 -0400

    Configura Passport

--- app/User.php
+++ app/User.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable implements MustVerifyEmail
 {
-    //
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'username', 'doct_type_id', 'rol_id', 'status_id', 'first_name', 'last_name', 'document', 'password'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
 }

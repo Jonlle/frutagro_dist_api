c21c6d7fe07fa493c5444e0278918ca5fe3d8792
Jonlle <jonlle19@gmail.com>
2020-03-08 04:26:23 -0400

    Crea rutas para auth

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ Route::post('login', 'API\AuthController@login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group(function(){
+    Route::post('details', 'API\AuthController@get_user_details_info');
+});

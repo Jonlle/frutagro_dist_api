77ea5978228ce40c9d8b995bc9479c00ff375833
Jonlle <jonlle19@gmail.com>
2020-03-08 21:10:01 -0400

    Modifica nombre de tabla role_permissions

--- database/migrations/2020_02_09_000014_create_role_permissions_table.php
+++ database/migrations/2020_02_09_000014_create_role_permissions_table.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesPermissionsTable extends Migration
+class CreateRolePermissionsTable extends Migration
 {
     /**
      * Schema table name to migrate
      * @var string
      */
-    public $tableName = 'roles_permissions';
+    public $tableName = 'role_permissions';
 
     /**
      * Run the migrations.
@@ -21,22 +21,22 @@ class CreateRolesPermissionsTable extends Migration
     public function up()
     {
         Schema::create($this->tableName, function (Blueprint $table) {
-            $table->increments('rol_perm_id');
+            $table->increments('role_perm_id');
             $table->string('permission_id', 10);
-            $table->string('rol_id', 6);
+            $table->string('role_id', 6);
 
-            $table->index(["permission_id"], 'fk_roles_permissions_permissions_idx');
+            $table->index(["permission_id"], 'fk_role_permissions_permissions_idx');
 
-            $table->index(["rol_id"], 'fk_roles_permissions_roles_idx');
+            $table->index(["role_id"], 'fk_role_permissions_roles_idx');
 
 
-            $table->foreign('permission_id', 'fk_role_permissionss_permissions')
+            $table->foreign('permission_id', 'fk_role_permissions_permissions')
                   ->references('permission_id')->on('permissions')
                   ->onDelete('restrict')
                   ->onUpdate('restrict');
 
-            $table->foreign('rol_id', 'fk_roles_permissions_roles')
-                  ->references('rol_id')->on('roles')
+            $table->foreign('role_id', 'fk_role_permissions_roles')
+                  ->references('role_id')->on('roles')
                   ->onDelete('restrict')
                   ->onUpdate('restrict');
         });
